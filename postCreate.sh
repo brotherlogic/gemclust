8827d448718d79bdf173e0a8c2ebc1fe6a3241ed
Simon Tucker <brotherlogic@gmail.com>
2026-04-12 00:20:52 -0700

    Fixes post Create

--- postCreate.sh
+++ postCreate.sh
@@ -1,5 +1,8 @@
-curl -sL https://talos.dev/install.sh | sudo sh
+curl -sL https://talos.dev/install | sudo sh
 curl -s https://fluxcd.io/install.sh | sudo bash
 git config --global user.email 'brotherlogic-automation@gmail.com'
 git config --global user.name 'Brotherlogic Automation'
 tic -x ghostty.terminfo
+
+# Install tmux and emacs
+sudo apt-get update && sudo apt-get install -y emacs
