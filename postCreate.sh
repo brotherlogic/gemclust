eaefc36d5e4915ba7d251795b4ca0ca5ca21fe73
Simon Tucker <brotherlogic@gmail.com>
2026-04-12 00:12:07 -0700

    Fix post create (#27)

---
+++ postCreate.sh
@@ -0,0 +1,5 @@
+curl -sL https://talos.dev/install.sh | sudo sh
+curl -s https://fluxcd.io/install.sh | sudo bash
+git config --global user.email 'brotherlogic-automation@gmail.com'
+git config --global user.name 'Brotherlogic Automation'
+tic -x ghostty.terminfo
